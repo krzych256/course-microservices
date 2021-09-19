b886765dd1f01c5c7e1f6ed5328bb7894fb22540
krzych256 <jasio93z@gmail.com>
2021-09-19 15:29:40 +0200

    Step 25 - Implementing Dynamic Filtering for RESTful Service

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/filtering/FilteringController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/filtering/FilteringController.java
@@ -1,5 +1,9 @@
 package com.in28minutes.rest.webservices.restfulwebservices.filtering;
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,13 +14,29 @@ import java.util.List;
 public class FilteringController {
 
     @GetMapping("/filtering")
-    public SomeBean retrieveSomeBean() {
-        return new SomeBean("value1", "value2", "value3");
+    public MappingJacksonValue retrieveSomeBean() {
+        SomeBean someBean = new SomeBean("value1", "value2", "value3");
+
+        return mappingJacksonValue(someBean, "SomeBeanFilter", new String[]{"field1", "field2"});
     }
 
     @GetMapping("/filtering-list")
-    public List<SomeBean> retrieveListOfSomeBean() {
-        return Arrays.asList(new SomeBean("value1", "value2", "value3"),
+    public MappingJacksonValue retrieveListOfSomeBean() {
+        List<SomeBean> someBeans = Arrays.asList(new SomeBean("value1", "value2", "value3"),
                 new SomeBean("value11", "value22", "value33"));
+
+        return mappingJacksonValue(someBeans, "SomeBeanFilter", new String[]{"field2", "field3"});
+    }
+
+    private MappingJacksonValue mappingJacksonValue(Object objectValue, String filterId, String[] fields) {
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept(fields);
+        FilterProvider filters = new SimpleFilterProvider()
+                .addFilter(filterId, filter);
+
+        MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(objectValue);
+        mappingJacksonValue.setFilters(filters);
+
+        return mappingJacksonValue;
     }
 }
