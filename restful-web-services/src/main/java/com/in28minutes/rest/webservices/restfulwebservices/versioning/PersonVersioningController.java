896f21de734c7b5b8312c4294608ea1ba66a5755
krzych256 <jasio93z@gmail.com>
2021-09-19 17:38:29 +0200

    Step 27 - Versioning RESTful Services - Header and Content Negotiation Approach

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/versioning/PersonVersioningController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/versioning/PersonVersioningController.java
@@ -15,4 +15,34 @@ public class PersonVersioningController {
     public PersonV2 personV2() {
         return new PersonV2(new Name("Bob", "Charlie"));
     }
+
+    @GetMapping(value = "/person/param", params = "version=1")
+    public PersonV1 personParamV1() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(value = "/person/param", params = "version=2")
+    public PersonV2 personParamV2() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
+    @GetMapping(value = "/person/header", headers = "X-API-VERSION=1")
+    public PersonV1 personHeaderV1() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(value = "/person/header", headers = "X-API-VERSION=2")
+    public PersonV2 personHeaderV2() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
+    @GetMapping(value = "/person/produces", produces = "application/vnd.company.app-v1+json")
+    public PersonV1 personProducesV1() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(value = "/person/produces", produces = "application/vnd.company.app-v2+json")
+    public PersonV2 personProducesV2() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
 }
