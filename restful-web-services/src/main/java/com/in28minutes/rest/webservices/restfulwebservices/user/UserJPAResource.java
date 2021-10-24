f73322fb0cec03afe54b7eab5cf54df3cd707253
krzych256 <jasio93z@gmail.com>
2021-10-24 20:31:14 +0200

    Step 31 - Updating GET methods on User Resource to use JPA

---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserJPAResource.java
@@ -0,0 +1,69 @@
+package com.in28minutes.rest.webservices.restfulwebservices.user;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@RestController
+public class UserJPAResource {
+
+    private final UserDaoService service;
+    private final UserRepository userRepository;
+
+    public UserJPAResource(UserDaoService service, UserRepository userRepository) {
+        this.service = service;
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/jpa/users")
+    public List<User> retrieveAllUsers() {
+        return userRepository.findAll();
+    }
+    
+    @GetMapping("/jpa/users/{id}")
+    public EntityModel<User> retrieveUser(@PathVariable int id) {
+        Optional<User> user = userRepository.findById(id);
+        
+        if (!user.isPresent())
+            throw new UserNotFoundException("id-" + id);
+
+        EntityModel<User> model = EntityModel.of(user.get());
+
+        WebMvcLinkBuilder linkToUsers =
+                linkTo(methodOn(this.getClass()).retrieveAllUsers());
+
+        model.add(linkToUsers.withRel("all-users"));
+        return model;
+    }
+    
+    @PostMapping("/jpa/users")
+    public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
+        User savedUser = service.save(user);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+        
+        return ResponseEntity.created(location).build();
+    }
+
+    @DeleteMapping("/jpa/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+        User user = service.deleteById(id);
+
+        if (user == null)
+            throw new UserNotFoundException("id-" + id);
+    }
+}
