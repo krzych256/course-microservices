ad72b80c45e6711a4694a193372245b4f887f844
krzych256 <jasio93z@gmail.com>
2022-11-29 16:31:26 +0100

    Step 15 - Creating Container Image for Currency Exchange Microservice

--- currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversion.java
+++ currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversion.java
@@ -3,81 +3,87 @@ package com.in28minutes.microservices.currencyconversionservice;
 import java.math.BigDecimal;
 
 public class CurrencyConversion {
+	private Long id;
+	private String from;
+	private String to;
+	private BigDecimal quantity;
+	private BigDecimal conversionMultiple;
+	private BigDecimal totalCalculatedAmount;
+	private String environment;
 
-    private Long id;
-    private String from;
-    private String to;
-    private BigDecimal quantity;
-    private BigDecimal conversionMultiple;
-    private BigDecimal totalCalculateAmount;
-    private String environment;
+	public CurrencyConversion() {
+		
+	}
+	
+	public CurrencyConversion(Long id, String from, String to, BigDecimal quantity, BigDecimal conversionMultiple, 
+			BigDecimal totalCalculatedAmount, String environment) {
+		super();
+		this.id = id;
+		this.from = from;
+		this.to = to;
+		this.conversionMultiple = conversionMultiple;
+		this.quantity = quantity;
+		this.totalCalculatedAmount = totalCalculatedAmount;
+		this.environment = environment;
+	}
 
-    public CurrencyConversion() {
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public CurrencyConversion(Long id, String from, String to, BigDecimal quantity, BigDecimal conversionMultiple, BigDecimal totalCalculateAmount, String environment) {
-        this.id = id;
-        this.from = from;
-        this.to = to;
-        this.conversionMultiple = conversionMultiple;
-        this.quantity = quantity;
-        this.totalCalculateAmount = totalCalculateAmount;
-        this.environment = environment;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public String getFrom() {
+		return from;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setFrom(String from) {
+		this.from = from;
+	}
 
-    public String getFrom() {
-        return from;
-    }
+	public String getTo() {
+		return to;
+	}
 
-    public void setFrom(String from) {
-        this.from = from;
-    }
+	public void setTo(String to) {
+		this.to = to;
+	}
 
-    public String getTo() {
-        return to;
-    }
+	public BigDecimal getConversionMultiple() {
+		return conversionMultiple;
+	}
 
-    public void setTo(String to) {
-        this.to = to;
-    }
+	public void setConversionMultiple(BigDecimal conversionMultiple) {
+		this.conversionMultiple = conversionMultiple;
+	}
 
-    public BigDecimal getConversionMultiple() {
-        return conversionMultiple;
-    }
+	public BigDecimal getQuantity() {
+		return quantity;
+	}
 
-    public void setConversionMultiple(BigDecimal conversionMultiple) {
-        this.conversionMultiple = conversionMultiple;
-    }
+	public void setQuantity(BigDecimal quantity) {
+		this.quantity = quantity;
+	}
 
-    public BigDecimal getQuantity() {
-        return quantity;
-    }
+	public BigDecimal getTotalCalculatedAmount() {
+		return totalCalculatedAmount;
+	}
 
-    public void setQuantity(BigDecimal quantity) {
-        this.quantity = quantity;
-    }
+	public void setTotalCalculatedAmount(BigDecimal totalCalculatedAmount) {
+		this.totalCalculatedAmount = totalCalculatedAmount;
+	}
 
-    public BigDecimal getTotalCalculateAmount() {
-        return totalCalculateAmount;
-    }
+	public String getEnvironment() {
+		return environment;
+	}
 
-    public void setTotalCalculateAmount(BigDecimal totalCalculateAmount) {
-        this.totalCalculateAmount = totalCalculateAmount;
-    }
+	public void setEnvironment(String environment) {
+		this.environment = environment;
+	}
+	
+	
+	
 
-    public String getEnvironment() {
-        return environment;
-    }
-
-    public void setEnvironment(String environment) {
-        this.environment = environment;
-    }
 }
--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchange.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchange.java
@@ -1,71 +1,78 @@
 package com.in28minutes.microservices.currencyexchangeservice;
 
+import java.math.BigDecimal;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import java.math.BigDecimal;
 
 @Entity
 public class CurrencyExchange {
+	
+	@Id
+	private Long id;
+	
+	@Column(name = "currency_from")
+	private String from;
+	
+	@Column(name = "currency_to")
+	private String to;
 
-    @Id
-    private Long id;
+	private BigDecimal conversionMultiple;
+	private String environment;
 
-    @Column(name = "currency_from")
-    private String from;
+	public CurrencyExchange() {
+		
+	}
+	
+	public CurrencyExchange(Long id, String from, String to, BigDecimal conversionMultiple) {
+		super();
+		this.id = id;
+		this.from = from;
+		this.to = to;
+		this.conversionMultiple = conversionMultiple;
+	}
 
-    @Column(name = "currency_to")
-    private String to;
-    private BigDecimal conversionMultiple;
-    private String environment;
+	public Long getId() {
+		return id;
+	}
 
-    public CurrencyExchange() {
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public CurrencyExchange(Long id, String from, String to, BigDecimal conversionMultiple) {
-        this.id = id;
-        this.from = from;
-        this.to = to;
-        this.conversionMultiple = conversionMultiple;
-    }
+	public String getFrom() {
+		return from;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public void setFrom(String from) {
+		this.from = from;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public String getTo() {
+		return to;
+	}
 
-    public String getFrom() {
-        return from;
-    }
+	public void setTo(String to) {
+		this.to = to;
+	}
 
-    public void setFrom(String from) {
-        this.from = from;
-    }
+	public BigDecimal getConversionMultiple() {
+		return conversionMultiple;
+	}
 
-    public String getTo() {
-        return to;
-    }
+	public void setConversionMultiple(BigDecimal conversionMultiple) {
+		this.conversionMultiple = conversionMultiple;
+	}
 
-    public void setTo(String to) {
-        this.to = to;
-    }
+	
+	public String getEnvironment() {
+		return environment;
+	}
 
-    public BigDecimal getConversionMultiple() {
-        return conversionMultiple;
-    }
+	public void setEnvironment(String environment) {
+		this.environment = environment;
+	}
+	
 
-    public void setConversionMultiple(BigDecimal conversionMultiple) {
-        this.conversionMultiple = conversionMultiple;
-    }
-
-    public String getEnvironment() {
-        return environment;
-    }
-
-    public void setEnvironment(String environment) {
-        this.environment = environment;
-    }
 }
